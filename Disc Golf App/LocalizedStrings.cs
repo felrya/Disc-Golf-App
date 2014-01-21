f020832fca3f73bf5571f14bef1ff73d9e45a66a
felrya <felrya92@live.com>
2014-01-21 02:04:59 -0600

    Project Start

---
+++ Disc Golf App/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Disc_Golf_App.Resources;
+
+namespace Disc_Golf_App
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
